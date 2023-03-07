831b09e28aed472c15b741d90992aba42c7d579d
czerwinski-adrian <adrianczerwinski3@gmail.com>
2023-03-07 12:23:19 +0000

    sequelize item model final

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const {Sequelize} = require('sequelize')
+const {Sequelize, DataTypes} = require('sequelize')
 const {sequelize} = require('../db')
 
 const Sauce = sequelize.define("sauces", {
@@ -8,11 +8,26 @@ const Sauce = sequelize.define("sauces", {
 
 //Adrian's code - Sequelize Model for Item
 const Item = sequelize.define("items", {
-  name: Sequelize.STRING,
-  description: Sequelize.STRING,
-  price: Sequelize.INTEGER,
-  category: Sequelize.STRING,
-  image: Sequelize.STRING
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull:false
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  image:{
+    type: DataTypes.STRING,
+    allowNull: false
+  }
 });
 
 //EXPORT
