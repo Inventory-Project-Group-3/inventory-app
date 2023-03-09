55c910ce7e74c60a0157c879a5ff263b7ff2c822
LeCodeMinister <ahmedalhindi@protonmail.com>
2023-03-09 11:12:39 +0000

    Delete & Put Requests Finished & Tested, Tier 3 & 4

--- server/routes/items.js
+++ server/routes/items.js
@@ -59,4 +59,56 @@ itemRouter.post("/", [
       }
 })
 
+//DELETE items/id/
+itemRouter.delete("/id/:id", async (req, res) => {
+  try {
+      const chosenItem = await Item.findByPk(req.params.id);
+      if (!chosenItem) {
+          throw new Error("No Item was found")
+      } else {
+          await chosenItem.destroy();
+          let myItems = await Item.findAll();
+          res.status(200).json(myItems);
+      }        
+  } catch (error) {
+      res.status(500).json({err: error.message})
+  }
+})
+
+
+//PUT items/id/
+itemRouter.put("/id/:id", [
+  check("name").not().isEmpty().trim(),
+  check("description").not().isEmpty().trim(),
+  check("price").not().isEmpty(),
+  check("category").not().isEmpty().trim(),
+  check("image").not().isEmpty().trim()], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      res.status(500).json({ error: errors.array()});
+  } else {
+      try {
+          const chosenItem = await Item.findByPk(req.params.id);
+          if (!chosenItem) {
+              throw new Error("No Item was found with this id")
+          } else {
+              await chosenItem.update({
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                category: req.body.category,
+                image: req.body.image
+              });
+              let myItems = await Item.findAll();
+              res.status(202).json(myItems);
+          }
+      } catch (error) {
+          res.status(500).json({err: error.message})
+      }
+  }
+})
+
+
+
+
 module.exports = itemRouter;
